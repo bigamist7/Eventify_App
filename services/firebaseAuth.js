7df53486ceb2122a3a85851da86f02121db2eef1
João Casanova Pinto <hello@jcasanova.pt>
2025-07-02 10:57:41 +0100

    Delete services/firebaseAuth.js

--- services/firebaseAuth.js
+++
@@ -1,8 +0,0 @@
-// services/firebaseAuth.js
-import firebase from "../firebaseConfig"; // ✅ firebase v8 usa import default
-
-export const signIn = (email, password) => {
-  const auth = firebase.auth?.(); // proteção extra
-  if (!auth) throw new Error("firebase.auth() está undefined");
-  return auth.signInWithEmailAndPassword(email, password);
-};
